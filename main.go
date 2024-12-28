94afafc310f18d6ae7596ffd64e2a5f24026c1bf
Kaamkiya <ziam3.1415@gmail.com>
2024-12-27 22:16:05 -0500

    refactor: Move license function to seperate file
    This just keeps the codebase cleaner.

--- main.go
+++ main.go
@@ -1,14 +1,7 @@
 package main
 
 import (
-	"strings"
-	"fmt"
-	"io"
-	"net/http"
-	"time"
-	"strconv"
 	"log"
-	"os"
 
 	"github.com/Iilun/survey/v2"
 )
@@ -134,35 +127,4 @@ func main() {
 			log.Printf("Failed to create LICENSE: %v\n", err)
 		}
 	}
-}
-
-func createLicense() error {
-	res, err := http.Get("https://raw.githubusercontent.com/github/choosealicense.com/refs/heads/gh-pages/_licenses/"+licenseType+".txt")
-	if err != nil {
-		return err
-	}
-	defer res.Body.Close()
-	if res.StatusCode > 299 || res.StatusCode < 200 {
-		return fmt.Errorf("Request failed with status %s", res.StatusCode)
-	}
-
-	f, err := os.Create("LICENSE")
-	if err != nil {
-		return err
-	}
-	defer f.Close()
-
-	data, err := io.ReadAll(res.Body)
-	if err != nil {
-		return err
-	}
-
-	text := strings.Split(string(data), "---")[2]
-	text = strings.ReplaceAll(text, "[year]", strconv.Itoa(time.Now().Year()))
-	text = strings.ReplaceAll(text, "[fullname]", licenseHolder)
-	text = strings.ReplaceAll(text, "[email]", licenserEmail)
-
-	f.WriteString(text)
-
-	return err
 }
